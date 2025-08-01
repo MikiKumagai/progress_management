9eedaeb5fcddfce2be124803ee9bbab39329c93e
Kumagai Miki <kumagai@tierline.com>
2025-08-01 16:39:17 +0900

    refresh task list

--- ui/pages/log_progress.py
+++ ui/pages/log_progress.py
@@ -64,27 +64,45 @@ class LogProgressPage(tk.Frame):
 
         self.after(0, self.set_default_task)
 
-    # TODO: リフレッシュして新しいタスクがリストにちゃんと入るようにする
-    def set_default_task(self):
-        if not self.tasks:
-            return
-        self.combo.current(0) 
-        self.selected_task_id = self.tasks[0][0]
-        progress_unit = progress_unit_service.get_progress_unit(self.selected_task_id)
-        progress_type = progress_type_service.get_progress_type(self.selected_task_id)
-        self.progress_unit.config(text=progress_unit)
-        self.progress_type.config(text=progress_type)
-    
-    def on_switch_task(self):
-        selected_index = self.combo.current()
-        if selected_index < 0:
-            return 
-        self.selected_task_id = self.tasks[selected_index][0]
-        progress_unit = progress_unit_service.get_progress_unit(self.selected_task_id)
-        progress_type = progress_type_service.get_progress_type(self.selected_task_id)
+    # データ取得共通メソッド
+    def update_task_labels(self, task_id):
+        progress_unit = progress_unit_service.get_progress_unit(task_id)
+        progress_type = progress_type_service.get_progress_type(task_id)
         self.progress_unit.config(text=progress_unit)
         self.progress_type.config(text=progress_type)
 
+    # TODO: リフレッシュして新しいタスクがリストにちゃんと入るようにする
+    def refresh(self):
+        self.tasks = task_service.get_tasks()
+        task_list = [task[1] for task in self.tasks]
+        self.combo['values'] = task_list
+
+        if task_list:
+            self.combo.current(0)
+            self.selected_task_id = self.tasks[0][0]
+            self.update_task_labels(self.selected_task_id)
+        else:
+            self.combo.set("")
+            self.progress_unit.config(text="")
+            self.progress_type.config(text="")
+    
+    # 初期表示
+    def set_default_task(self):
+        if not self.tasks:
+            return
+        self.combo.current(0)
+        self.selected_task_id = self.tasks[0][0]
+        self.update_task_labels(self.selected_task_id)
+    
+    # 切り替えボタン押下
+    def on_switch_task(self):
+        selected_index = self.combo.current()
+        if selected_index < 0:
+            return
+        self.selected_task_id = self.tasks[selected_index][0]
+        self.update_task_labels(self.selected_task_id)
+    
+    # 登録ボタン押下
     def on_submit(self):
         selected_index = self.combo.current()
         task_id = self.tasks[selected_index][0]

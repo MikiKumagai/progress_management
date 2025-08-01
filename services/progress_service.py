eb4907b4da3ca0bd7d1c3a5f00827789af438165
Kumagai Miki <kumagai@tierline.com>
2025-08-01 22:51:47 +0900

    improve add_progress function

--- services/progress_service.py
+++ services/progress_service.py
@@ -2,25 +2,27 @@ from models import task_model, progress_model
 
 # 進捗記録ページ：進捗記録
 def add_progress(task_id, progress_value, progress_type):
-    # 1. 進捗をprogressテーブルに追加
     progress, total_count = task_model.select_task_data(task_id)
-    print("progress: ", progress)
-    print("total_count: ", total_count)
-    progress_total = 0
-    progress_diff = 0
+    progress_total, progress_diff = calculate_progress(progress, progress_value, progress_type)
+    # 進捗を登録
+    if progress_model.select_today_data(task_id) == 1:
+        progress_model.insert_progress(task_id, progress_diff)
+    else:
+        progress_model.update_progress(task_id, progress_diff)
+    # taskの進捗情報を更新
+    task_model.update_task_progress(task_id, progress_total)
+    # task完了しているかチェック
+    if progress_total == total_count:
+        task_model.update_task_completion(task_id)
+
+# 進捗記録ページ：進捗記録データの計算
+def calculate_progress(progress, progress_value, progress_type):
     if progress_type == "累計":
         progress_total = progress_value
         progress_diff = progress_value - progress
     elif progress_type == "差分":
         progress_total = progress_value + progress
         progress_diff = progress_value
-    print("after_estimate_total: ", progress_total)
-    print("after_estimate_diff: ", progress_diff)
-    progress_model.insert_progress(task_id, progress_diff)
-    # 2. 合計進捗を更新
-    task_model.update_task_progress(task_id, progress_total)
-    # 3. total_countとprogressが同じになったらactiveをFalseにする
-    if progress_total == total_count:
-        task_model.update_task_completion(task_id)
-
-# 日に2回送られてきたときどうしよかな
+    else:
+        raise ValueError("不明な入力タイプ")
+    return progress_total, progress_diff

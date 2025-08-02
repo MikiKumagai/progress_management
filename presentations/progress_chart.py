9475e9d250e5aef7f2a697aea90145698d60bbde
Kumagai Miki <kumagai@tierline.com>
2025-08-02 20:34:38 +0900

    remaining

--- presentations/progress_chart.py
+++ presentations/progress_chart.py
@@ -9,7 +9,6 @@ def create_progress_chart(task_id):
     # データ取得
     raw_data = progress_model.select_progresses_for_chart(task_id)
     task_name, total_count = task_model.select_task_for_chart(task_id)
-    # TODO: 残数のグラフにする
     # 必要な情報を整える
     df = pd.DataFrame(raw_data, columns=["task_name", "task_id", "progress_value", "progress_date"])
     df = df[df["task_name"] == task_name].copy()
@@ -18,12 +17,15 @@ def create_progress_chart(task_id):
     today = pd.to_datetime(date.today())
     df = df[df["progress_date"] <= today]
     # 累積を計算
+    # TODO: 残数のグラフにする
     df = df.sort_values("progress_date")
     df["cumulative_progress"] = df["progress_value"].cumsum()
+    df["remaining_value"] = total_count - df["cumulative_progress"]
 
     fig = Figure(figsize=(6, 4), dpi=100)
     ax = fig.add_subplot(111)
-    ax.plot(df["progress_date"], df["cumulative_progress"])
+    ax.plot(df["progress_date"], df["remaining_value"])
     ax.xaxis.set_major_locator(AutoDateLocator())
+    ax.set_ylim(bottom=0)
     fig.autofmt_xdate()
     return fig
